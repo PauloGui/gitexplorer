38bb8ac2d657eb16f521fa97c923e622f510da5a
Paulo Sousa <paulopgss@gmail.com>
2020-11-13 11:26:48 -0300

    Estilizando detalhes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, ReposityInfo, Issues } from './style';
+import Logo from '../../assets/image/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={Logo} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <ReposityInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>Nome do repositorio</strong>
+            <p>Descricao do repositorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </ReposityInfo>
+
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>puhoshs</strong>
+            <p>aishdoihasd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
